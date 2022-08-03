f6d40303d9a360ccc534fee94dbf6d1c84b4d0d4
Nuno <nuno.carmo@mindswap.academy>
2022-08-03 14:53:11 +0100

    Search filters and semi search page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Login from "./pages/Login";
-import Header from "./components/Header";
-
+/* import Login from "./pages/Login";
+import Header from "./components/Header"; */
+import Search from "./pages/Search";
 function App() {
   return (
     <>
-      <Header />
-      <Login />
+      <Search />
+      {/*       <Header />
+      <Login /> */}
     </>
   );
 }
