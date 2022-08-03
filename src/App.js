4720fe0460c3f81f7c1ad41ce547bc44ac442fad
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-03 15:37:18 +0100

    creation of the router file and all project routes. Menu component creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Login from "./pages/Login";
 import Header from "./components/Header";
 import Register from "./pages/Register";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <>
+      <Menu />
       <Header />
       <Login />
-      <Register/>
+      <Register />
     </>
   );
 }
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const ContainerMenu = styled.div`
+  background-color: #fff;
+  border-bottom: solid 1px #fdc544;
+`;
+
+export const Links = styled(Link)`
+    display: block;
+`;
