18c25f5fad70681ac8d6682ef5c68d66a55c0fbd
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-03 17:19:54 +0100

    change in header design and book component adjustments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,19 @@
 import React from "react";
-// import {} from "./styles";
+import { ContainerBooks } from "./styles";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Book from "../../components/Book";
 import Livro from "../../assets/Livro.jpg";
 
-//chamar o footer, header, etc aqui
 const Home = () => {
   return (
     <>
       <Header />
-      <Book image={Livro}></Book>
+      <ContainerBooks>
+        <Book image={Livro}></Book>
+        <Book image={Livro}></Book>
+        <Book image={Livro}></Book>
+      </ContainerBooks>
       <Footer />
     </>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  background-color: #1c1c1c;
-  max-width: 80%;
-  margin: auto;
-  padding: 16px 10px;
-  border-radius: 14px;
-  border: solid 1px #a9a9a9;
+export const ContainerBooks = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
 `;
