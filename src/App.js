8a61c5f7d69df9c079faa086efd8592900c4da85
Nuno <nuno.carmo@mindswap.academy>
2022-08-03 14:57:32 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-/* import Login from "./pages/Login";
-import Header from "./components/Header"; */
+import Login from "./pages/Login";
+import Header from "./components/Header";
+import Register from "./pages/Register";
 import Search from "./pages/Search";
 function App() {
   return (
     <>
       <Search />
-      {/*       <Header />
-      <Login /> */}
-=======
-import Login from "./pages/Login";
-import Header from "./components/Header";
-import Register from "./pages/Register";
-
-function App() {
-  return (
-    <>
       <Header />
       <Login />
-      <Register/>
->>>>>>> 11de04e7fa895b1f2166f20029d5b8ea6a400205
+      <Register />
     </>
   );
 }
