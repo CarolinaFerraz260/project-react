b0141fabd28a060492f1f35af119171a0e91e4bf
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-04 17:17:59 +0100

    add button login and button delete book

--- src/pages/ChangeProfile/index.js
+++ src/pages/ChangeProfile/index.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Livro from "../../assets/Livro.jpg";
-<<<<<<< HEAD
-=======
-import { useLocation, useNavigate } from "react-router-dom";
->>>>>>> 88e17ecb574aabf90aef8e8e8f69251ac1f55c39
 import {
   ContainerProfile,
   ContainerInfosUser,
@@ -19,99 +15,40 @@ import {
 } from "./styles";
 
 const ChangeProfile = () => {
-  const navigate = useNavigate();
-  const info = useLocation();
-  const user = info.state;
-
-  useEffect(() => {
-    document.querySelector("#chname").value = user.name;
-    document.querySelector("#chemail").value = user.email;
-    document.querySelector("#chimg").value = user.profile_picture;
-  }, []);
-
-
-  async function updateUser(e) {
-    const token = document.cookie
-      .split("; ")
-      .map((a) => a.split("="))
-      .filter(([a, b]) => a === "token")
-      .flat();
-    e.preventDefault();
-    const newName = e.target.querySelector("#chname").value;
-    const newEmail = e.target.querySelector("#chemail").value;
-    const newImgLink = e.target.querySelector("#chimg").value;
-    const newPassword = e.target.querySelector("#chpassword").value;
-    const updatedUser = {
-      name: newName,
-      email: newEmail,
-      profile_picture: newImgLink
-    }
-    if (newPassword !== "") updatedUser.password = newPassword;
-    try {
-      await fetch("/api/user/profile", {
-        method: "PUT",
-        headers: new Headers({
-          "Content-Type": "application/json",
-          Authorization: token[1]
-        }),
-        body: JSON.stringify(updatedUser),
-      });
-      navigate("../profile")
-    } catch (err) {
-    }
-  }
-
   return (
     <>
       <Header />
       <ContainerProfile>
-        <ContainerInfosUser onSubmit={updateUser}>
+        <ContainerInfosUser>
           <ContainerImageUser>
-            <ImageUser src={user.profile_picture} />
+            <ImageUser src={Livro} />
           </ContainerImageUser>
           <ContainerInputs>
             <label>
-              <InputChangeImage name="nameUser" type="text" id="chimg" placeholder="Img Link" required></InputChangeImage>
+              Choose
+              <InputChangeImage name="nameUser" type="file"></InputChangeImage>
             </label>
             <InputEditProfile
               placeholder="User name"
               name="nameUser"
               type="text"
-<<<<<<< HEAD
             />
-=======
-              id="chname"
-              required
-            ></InputEditProfile>
->>>>>>> 88e17ecb574aabf90aef8e8e8f69251ac1f55c39
             <InputEditProfile
               placeholder="Email"
               name="emailUser"
               type="email"
-<<<<<<< HEAD
             />
-=======
-              id="chemail"
-              required
-            ></InputEditProfile>
->>>>>>> 88e17ecb574aabf90aef8e8e8f69251ac1f55c39
             <InputEditProfile
               placeholder="Password"
               name="passwUser"
               type="password"
-<<<<<<< HEAD
             />
-=======
-              id="chpassword"
-            ></InputEditProfile>
->>>>>>> 88e17ecb574aabf90aef8e8e8f69251ac1f55c39
           </ContainerInputs>
-          <ContainerButtonSave>
-            <ButtonSaveProfile type="submit">Save</ButtonSaveProfile>
-          </ContainerButtonSave>
         </ContainerInfosUser>
       </ContainerProfile>
-
+      <ContainerButtonSave>
+        <ButtonSaveProfile>Save</ButtonSaveProfile>
+      </ContainerButtonSave>
       <Footer />
     </>
   );
