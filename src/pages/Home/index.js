1688e438faecf4315a368429eaabf4131dbc3ad1
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-04 11:39:23 +0100

    change in the README file and in the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,8 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Book from "../../components/Book";
 import Livro from "../../assets/Livro.jpg";
+import Livro2 from "../../assets/Livro2.jpg";
+import Livro3 from "../../assets/Livro3.jpg";
 
 const Home = () => {
   return (
@@ -12,8 +14,8 @@ const Home = () => {
       <ContainerBooks>
         {/* TODO colocar o livro correto aparecer */}
         <Book image={Livro}></Book>
-        <Book image={Livro}></Book>
-        <Book image={Livro}></Book>
+        <Book image={Livro2}></Book>
+        <Book image={Livro3}></Book>
       </ContainerBooks>
       <Footer />
     </>
