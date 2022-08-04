0093f0fb70ce76acdb18337a29a9a4eb050829c2
Carolina Pereira <carolinapereira750@gmail.com>
2022-08-04 11:22:49 +0100

    cleaning in the project structure and its folders. change in design

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,15 +3,14 @@ import { ContainerBooks } from "./styles";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Book from "../../components/Book";
-import Menu from "../../components/Menu";
 import Livro from "../../assets/Livro.jpg";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Menu />
       <ContainerBooks>
+        {/* TODO colocar o livro correto aparecer */}
         <Book image={Livro}></Book>
         <Book image={Livro}></Book>
         <Book image={Livro}></Book>
--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,19 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
-import AllBooks from "../pages/AllBooks";
 import MyBooks from "../pages/MyBooks";
-import Favorites from "../pages/Favorites";
 import Search from "../pages/Search";
 import Profile from "../pages/Profile";
-import Menu from "../components/Menu";
 
 const Router = ({ handleSubmit }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Menu />} />
+        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login handleSubmit={handleSubmit} />} />
         <Route path="register" element={<Register />} />
         <Route path="home" element={<Home />} />
-        <Route path="allBooks" element={<AllBooks />} />
         <Route path="myBooks" element={<MyBooks />} />
-        <Route path="favorites" element={<Favorites />} />
         <Route path="search" element={<Search />} />
         <Route path="profile" element={<Profile />} />
       </Routes>
